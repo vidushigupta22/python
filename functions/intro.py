759ecb941458d1b1e5bce58a7bb17628d393848d
vidushi gupta <92041996+vidushigupta22@users.noreply.github.com>
2022-06-05 17:05:17 +0530

    more files

---
+++ functions/intro.py
@@ -0,0 +1,21 @@
+
+# program that calculate hypotenuse
+
+# functions are defined by def keyword
+# and then we can use these defined function by calling them
+
+def hypotenuse():
+    p = float(input('enter the length of first side'))
+
+    b = float(input('enter the length of second side'))
+
+    h = (p**2 + b**2)**0.5
+
+    print('perpendicular', p)
+    print('base', b)
+    print('hypotenuse', h)
+
+
+
+#calling
+hypotenuse()
